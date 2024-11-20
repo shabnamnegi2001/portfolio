c77418dd6e256c60ab904ca05de163b048cdbb07
Shabbi <negishabnam37@gmail.com>
2024-11-20 12:21:36 +0530

    mnor changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,23 @@ import LeftLine from './components/LeftLine';
 function App() {
 
   return (
-   <>
+   
+  <div className='grid grid-flow-col grid-cols-2 h-full w-full '>
+
+   <div className='w-full h-full'>
+    leftsidebar
+   </div>
+   <div className='w-full h-full'>
    <Navbar />
    <div className='grid-cols-1 w-full'>
     <LeftLine/>
    <Home />
   </div>
-   </>
+  </div>
+  </div>
+
   )
 }
 
 export default App
+
